dc2ceac6512cc898fd67a92581402b23a9a2d634
Jerry M <developer.jerrymannel@gmail.com>
2021-04-03 09:45:46 +0530

    Starting to generate tests

--- routes/testSuite.routes.js
+++ routes/testSuite.routes.js
@@ -2,11 +2,15 @@ const MongooseExpressMiddleware = require("mongoose-express-middleware")
 const express = require('express');
 const router = express.Router();
 
+const apiClient = require('../lib/api.client')
+const generator = require('../lib/generator')
+
 let schema = require("../schema/testSuite.schema")
 
 const testsCrud = new MongooseExpressMiddleware("testSuite", schema, null)
 
-router.post("", testsCrud.create)
+let logger = global.logger;
+
 router.get("", testsCrud.index)
 router.get("/bulkShow", testsCrud.bulkShow)
 router.put("/bulkUpdate", testsCrud.bulkUpdate)
@@ -14,5 +18,18 @@ router.delete("/bulkDelete", testsCrud.bulkDestroy)
 router.get("/:id", testsCrud.show)
 router.put("/:id", testsCrud.update)
 router.delete("/:id", testsCrud.destroy)
+
+router.post("", async (_req, _res) => {
+	try {
+		const data = _req.body;
+		logger.trace(data);
+		// let response = await testsCrud.model(data).save();
+		generator.generate(data);
+		_res.json(data);
+		// _res.json(response);
+	} catch (_err) {
+		apiClient.handleError(_err, _res);
+	}
+})
 
 module.exports = router
