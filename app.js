59d27e909e07e9409e3e48d241949015c0d52ffd
Jerry M <developer.jerrymannel@gmail.com>
2021-04-30 11:56:14 +0530

    Added file validation

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const config = require('./config');
 const log4js = require('log4js');
 log4js.configure(config.logging.options);
@@ -6,6 +5,8 @@ let version = require('./package.json').version;
 let logger = log4js.getLogger(`[DAT ${version}]`);
 logger.level = config.logging.loglevel;
 global.logger = logger;
+
+const fileValidator = require('./lib/filevalidator')
 
 if (process.argv.length < 3) {
 	console.log('Missing test file');
@@ -16,10 +17,4 @@ const testFile = process.argv[2];
 
 logger.info(`Using test file : ${testFile}`);
 
-if (!fs.existsSync(testFile)) {
-	logger.error(`Unable to find test file ${testFile}`);
-}
-
-const testFileData = fs.readFileSync(testFile).toString();
-
-logger.info(testFileData);
+fileValidator(testFile);
